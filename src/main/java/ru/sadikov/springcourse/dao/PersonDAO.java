b3464e13e9a031d68a61151d139605ddac411f2c
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-10-13 17:20:38 +0500

    result

--- src/main/java/ru/sadikov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/sadikov/springcourse/dao/PersonDAO.java
@@ -3,26 +3,45 @@ package ru.sadikov.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.sadikov.springcourse.models.Person;
 
+import java.sql.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Vitaly"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static Connection connection;
+    static {
+        try {
+            Class.forName("org.postgressql.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
     public List<Person> index(){
-        return people;
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Person show(int id){
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person){
@@ -30,8 +49,8 @@ public class PersonDAO {
         people.add(person);
     }
     public void update(int id, Person updatePerson){
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
+        //Person personToBeUpdated = show(id);
+        //personToBeUpdated.setName(updatePerson.getName());
 
     }
 
